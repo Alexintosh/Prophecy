101f13ceb4259ad6ef19ee8fcb021ffe8609500d
Alessio <alessio.d@gmail.com>
2017-08-17 09:05:48 +0200

    Recharts added

--- src/containers/ExamplePages/TabPage.js
+++ src/containers/ExamplePages/TabPage.js
@@ -5,32 +5,38 @@ import { getAccountsFromWIFKey, doSendAsset, generatePrivateKey, getWIFFromPriva
 import TransactionTab from './TransactionTab';
 import TransactionList from './TransactionList';
 import { Balance } from '../../components/Balance';
+import {AreaChart, LineChart, Line, XAxis, Area, YAxis, CartesianGrid, Tooltip, Legend} from 'Recharts';
+const data = [
+      {name: 'june', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'july', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'aug', uv: 2000, pv: 9800, amt: 2290}
+];
 
 const pkey = 'AchEwoFAMiR2hph3FTKqoHiuBQ3f3qpmqz';
 
 class TabPage extends React.Component {
 
   // componentDidMount() {
-    // const assetType = 'Gas';
-    // const amount = 0.01;
-    // doSendAsset('TestNet', toAddress, fromWif, assetType, amount)
-    // .then((response) => {
-    //   console.log("RS", response);
-    //   if (response.result === undefined){
-    //     console.log("Transaction failed!");
-    //   } else {
-    //     console.log("Transaction complete! Your balance will automatically update when the blockchain has processed it.")
-    //   }
-    // }).catch((e) => {
-    //   console.log("Transaction failed!");
-    // });
+  // const assetType = 'Gas';
+  // const amount = 0.01;
+  // doSendAsset('TestNet', toAddress, fromWif, assetType, amount)
+  // .then((response) => {
+  //   console.log("RS", response);
+  //   if (response.result === undefined){
+  //     console.log("Transaction failed!");
+  //   } else {
+  //     console.log("Transaction complete! Your balance will automatically update when the blockchain has processed it.")
+  //   }
+  // }).catch((e) => {
+  //   console.log("Transaction failed!");
+  // });
   // }
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { 
-        transactionHistory: [],
-        state: 'initial',
+    this.state = {
+      transactionHistory: [],
+      state: 'initial',
     };
   }
 
@@ -52,12 +58,24 @@ class TabPage extends React.Component {
 
   render() {
     return (
-      <Page renderToolbar={() => <Toolbar title={this.props.title} />} >
+      <Page style={{backgroundColor:'#103F7F', color:'#fff'}} renderToolbar={() => <Toolbar title={this.props.title} />} >
+        <div style={{backgroundColor:'#103F7F', color:'#fff'}}>
         <Balance />
+        <AreaChart width={window.innerWidth} height={200} data={data}
+          margin={{ top: 5, right: 5, left: 5, bottom: 5 }}>
+          <XAxis dataKey="name" stroke="#c0ceb3" tickLine={{stroke: '#fff', strokeWidth: 0.5}} tick={{stroke: '#fff', strokeWidth: 0.5}}/>
+          <YAxis axisLine={false} stroke="#c0ceb3" mirror={true} tick={{stroke: '#fff', strokeWidth: 0.5}}/>
+          
+          <Tooltip />
+          <Line type="monotone" dataKey="pv" stroke="#fff" activeDot={{ r: 8 }} />
+          <Area type='monotone' dataKey='pv' stroke='#fff' fill='rgba(255, 255, 255, 0.4)' tick={{stroke: '#fff', strokeWidth: 0.5}} />
+          
+        </AreaChart>
         <TransactionList
-            key="tab_history"
-            history={this.state.transactionHistory}
+          key="tab_history"
+          history={this.state.transactionHistory}
         />
+        </div>
       </Page>
     );
   }
